186c72dff1d817c1494b3b93cae00ff0b1e0caaa
JSM85 <jsmendonca@gmail.com>
2014-05-02 18:20:01 +0100

    lecture 31-34

---
+++ Lecture_31_CreatingControllers/app/controllers/customersController.js
@@ -0,0 +1,16 @@
+function CustomersController($scope) {
+
+    $scope.sortBy = 'name';
+    $scope.reverse = false;
+
+    $scope.customers = [
+                {name: 'John',city:'London', orderTotal:'9.9539', joined: '2009-03-01'},
+                {name: 'Joe',city:'NYC', orderTotal:'3.9539',joined: '2009-03-01'},
+                {name: 'Albert',city:'Tokyo', orderTotal:'5.9539',joined: '2009-03-01'}
+    ];
+
+    $scope.doSort = function(propName){
+        $scope.sortBy=propName;
+        $scope.reverse=!$scope.reverse;
+    };
+}
