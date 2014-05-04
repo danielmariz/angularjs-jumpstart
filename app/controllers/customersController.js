88edadc8db9a854ac17827ed2c12655c5c6bb1d0
JSM85 <jsmendonca@gmail.com>
2014-05-04 08:33:59 +0100

    End of module cleanup

--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -1,70 +1,39 @@
-//Option 1
+(function () {
 
-
-/*
-app.controller('CustomersController', function ($scope) {
-
-    $scope.sortBy = 'name';
-    $scope.reverse = false;
-
-    $scope.customers = [
-                {name: 'John',city:'London', orderTotal:'9.9539', joined: '2009-03-01'},
-                {name: 'Joe',city:'NYC', orderTotal:'3.9539',joined: '2009-03-01'},
-                {name: 'Albert',city:'Tokyo', orderTotal:'5.9539',joined: '2009-03-01'}
-    ];
-
-    $scope.doSort = function(propName){
-        $scope.sortBy=propName;
-        $scope.reverse=!$scope.reverse;
-    };
-});
-*/
-
-// Option 2
-/*(function() {
-    angular.module('customersApp')
-    .controller('CustomersController', function ($scope) {
-
-        $scope.sortBy = 'name';
-        $scope.reverse = false;
-
-        $scope.customers = [
-                    {name: 'John',city:'London', orderTotal:'9.9539', joined: '2009-03-01'},
-                    {name: 'Joe',city:'NYC', orderTotal:'3.9539',joined: '2009-03-01'},
-                    {name: 'Albert',city:'Tokyo', orderTotal:'5.9539',joined: '2009-03-01'}
-        ];
-
-        $scope.doSort = function(propName){
-            $scope.sortBy=propName;
-            $scope.reverse=!$scope.reverse;
-        };
-    });
-}());
-*/
-
-// Option 3
-
-(function() {
-    
     var CustomersController = function ($scope) {
 
         $scope.sortBy = 'name';
         $scope.reverse = false;
 
         $scope.customers = [
-                    {name: 'John',city:'London', orderTotal:'9.9539', joined: '2009-03-01'},
-                    {name: 'Joe',city:'NYC', orderTotal:'3.9539',joined: '2009-03-01'},
-                    {name: 'Albert',city:'Tokyo', orderTotal:'5.9539',joined: '2009-03-01'}
+            {
+                name: 'John',
+                city: 'London',
+                orderTotal: '9.9539',
+                joined: '2009-03-01'
+            },
+            {
+                name: 'Joe',
+                city: 'NYC',
+                orderTotal: '3.9539',
+                joined: '2009-03-01'
+            },
+            {
+                name: 'Albert',
+                city: 'Tokyo',
+                orderTotal: '5.9539',
+                joined: '2009-03-01'
+            }
         ];
 
-        $scope.doSort = function(propName){
-            $scope.sortBy=propName;
-            $scope.reverse=!$scope.reverse;
+        $scope.doSort = function (propName) {
+            $scope.sortBy = propName;
+            $scope.reverse = !$scope.reverse;
         };
     };
-    
-    CustomersController.$inject = ['$scope']
-    
+
+    CustomersController.$inject = ['$scope'];
+
     angular.module('customersApp')
         .controller('CustomersController', CustomersController);
 }());
