643d8a5187175712b7a0099e026f0aabb257fb0c
JSM85 <jsmendonca@gmail.com>
2014-05-04 11:46:57 +0100

    Lecture 56 - beginning to put common shared code into factories

--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -6,49 +6,6 @@
         $scope.sortBy = 'name';
         $scope.reverse = false;
 
-        $scope.customers = [
-            {
-                id: 1,
-                name: 'John',
-                city: 'London',
-                orderTotal: '9.9539',
-                joined: '2009-02-07',
-                orders: [{
-                    id:1,
-                    product: 'Shoes',
-                    total: 9.9539
-                }]
-            },
-            {
-                id: 2,
-                name: 'Joe',
-                city: 'NYC',
-                orderTotal: '3.9539',
-                joined: '2009-03-01',
-                orders:[{
-                    id:2,
-                    product: 'Book',
-                    total: 3.9539
-                }]
-            },
-            {
-                id: 3,
-                name: 'Albert',
-                city: 'Tokyo',
-                orderTotal: '10',
-                joined: '2009-04-21',
-                orders :[{
-                    id:3,
-                    product: 'Ring',
-                    total: 6
-                },{
-                    id:4,
-                    product: 'Box',
-                    total: 4
-                }]
-            }
-        ];
-
         $scope.doSort = function (propName) {
             $scope.sortBy = propName;
             $scope.reverse = !$scope.reverse;
---
+++ app/services/customersFacroty.js
@@ -0,0 +1,55 @@
+(function(){
+    var customerFactory = function(){
+        var customers = [
+            {
+                id: 1,
+                name: 'John',
+                city: 'London',
+                orderTotal: '9.9539',
+                joined: '2009-02-07',
+                orders: [{
+                    id:1,
+                    product: 'Shoes',
+                    total: 9.9539
+                }]
+            },
+            {
+                id: 2,
+                name: 'Joe',
+                city: 'NYC',
+                orderTotal: '3.9539',
+                joined: '2009-03-01',
+                orders:[{
+                    id:2,
+                    product: 'Book',
+                    total: 3.9539
+                }]
+            },
+            {
+                id: 3,
+                name: 'Albert',
+                city: 'Tokyo',
+                orderTotal: '10',
+                joined: '2009-04-21',
+                orders :[{
+                    id:3,
+                    product: 'Ring',
+                    total: 6
+                },{
+                    id:4,
+                    product: 'Box',
+                    total: 4
+                }]
+            }
+        ];
+        
+        var factory = {};
+        factory.getCutsomers = function(){
+            return customers;
+        };
+        
+        return factory;
+    };
+    
+    angular.module('customersApp').factory('customersFactory', customerFactory);
+});
