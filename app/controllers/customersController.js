83feef1271bb3d518617cba4b942e72a37bdf8c1
JSM85 <jsmendonca@gmail.com>
2014-05-04 08:28:13 +0100

    Lecture 38 - Option 3 (I like this solution best)

--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -1,4 +1,4 @@
-//Option 2
+//Option 1
 
 
 /*
@@ -20,7 +20,8 @@ app.controller('CustomersController', function ($scope) {
 });
 */
 
-(function() {
+// Option 2
+/*(function() {
     angular.module('customersApp')
     .controller('CustomersController', function ($scope) {
 
@@ -38,4 +39,30 @@ app.controller('CustomersController', function ($scope) {
             $scope.reverse=!$scope.reverse;
         };
     });
+}());
+*/
+
+// Option 3
+
+(function() {
+    
+    var CustomersController = function ($scope) {
+
+        $scope.sortBy = 'name';
+        $scope.reverse = false;
+
+        $scope.customers = [
+                    {name: 'John',city:'London', orderTotal:'9.9539', joined: '2009-03-01'},
+                    {name: 'Joe',city:'NYC', orderTotal:'3.9539',joined: '2009-03-01'},
+                    {name: 'Albert',city:'Tokyo', orderTotal:'5.9539',joined: '2009-03-01'}
+        ];
+
+        $scope.doSort = function(propName){
+            $scope.sortBy=propName;
+            $scope.reverse=!$scope.reverse;
+        };
+    };
+    
+    angular.module('customersApp')
+        .controller('CustomersController', CustomersController);
 }());
