effe36380879a5e6892b9d53ae523f249afe2de3
JSM85 <jsmendonca@gmail.com>
2014-05-05 23:36:01 +0100

    Lecture 71 - Added Bootstrap

--- app/app.js
+++ app/app.js
@@ -9,10 +9,15 @@
                   controller: 'CustomersController',
                   templateUrl: 'app/views/Customers.html'
               })
-                .when('/orders/:customerId',
+        .when('/orders/:customerId',
               {
                   controller: 'OrdersController',
                   templateUrl: 'app/views/Orders.html'
+              })
+        .when('/orders',
+              {
+                  controller: 'AllOrdersController',
+                  templateUrl: 'app/views/AllOrders.html'
               })
         .otherwise({ redirectTo: '/'});
     });
